5e5077f12e484427160b78ce1be93057e26e7eb6
Ajay K <76083469+ajaypep@users.noreply.github.com>
2024-01-16 23:00:19 +0530

    feat!: Add script.js and align text of display

---
+++ script.js
@@ -0,0 +1,18 @@
+const display = document.querySelector('#display');
+
+function handleNumButtons() {
+    const numButtons = document.querySelectorAll('.num');
+    numButtons.forEach(btn => btn.addEventListener(
+        'click',
+        () => {
+            if (display.textContent.length >= 5) {
+                alert('Number must be below 10^5');
+            }
+            else {
+                display.textContent = Number(display.textContent) * 10 + Number(btn.textContent);
+            }
+        }
+    ));
+}
+
+handleNumButtons();
